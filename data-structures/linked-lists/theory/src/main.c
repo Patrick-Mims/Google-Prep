4e4c7ff63b2551948e6c54c9ca1035ac6e04550c
Patrick Mims <patrickkevinmims@gmail.com>
2022-01-13 17:16:19 -0800

    First Step in setting up a linked-list

---
+++ data-structures/linked-lists/theory/src/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+  int item;
+  struct node *next;
+};
+
+int main(int argc, char **argv)
+{
+    struct node *head = NULL;
+    if((head = malloc(sizeof(struct node))) == NULL)
+      exit(EXIT_FAILURE);
+
+    struct node *new_node = NULL;
+    if((new_node = malloc(sizeof(struct node))) == NULL)
+      exit(EXIT_FAILURE);
+
+    (*new_node).item = 30;
+
+    new_node->next = head;
+    head = new_node;
+
+  return 0;
+}
