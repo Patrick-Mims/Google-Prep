b04a3ead94d9ec0ac646e034c1ab7c554cb476e1
Patrick Mims <patrickkevinmims@gmail.com>
2022-02-08 10:21:46 -0800

    latest

--- data-structures/stacks/generic-stack/inc/stack.h
+++ data-structures/stacks/generic-stack/inc/stack.h
@@ -3,53 +3,58 @@
 
 struct stackNode
 {
-    int data;
-    struct stackNode *nextPtr;
+  int data;
+  struct stackNode *nextPtr;
 };
 
 void display(struct stackNode *top)
 {
-    if (top != NULL)
+  if (top != NULL)
+  {
+    while (top != NULL)
     {
-        while (top != NULL)
-        {
-            printf("-> %d\n", top->data);
-            top = top->nextPtr;
-        }
+      printf("-> %d\n", top->data);
+      top = top->nextPtr;
     }
-    else
-    {
-        printf("Empty Stack");
-    }
+  }
+  else
+  {
+    printf("Empty Stack");
+  }
 }
 
 /* pushing onto the stack, again using double pointer */
 void push(struct stackNode **top, int value)
 {
-    struct stackNode *newNode = NULL;
+  // create a new node to insert
+  struct stackNode *newNode = NULL;
 
-    if ((newNode = malloc(sizeof(struct stackNode))) == NULL)
-    {
-        exit(EXIT_FAILURE);
-    }
+  /* malloc memory for the new node*/
+  if ((newNode = malloc(sizeof(struct stackNode))) == NULL)
+  {
+    exit(EXIT_FAILURE);
+  }
 
-    newNode->data = value;
-    newNode->nextPtr = *top;
-    *top = newNode;
+  /* assign value to data */
+  newNode->data = value;
+  newNode->nextPtr = *top;
+
+  /* return the pointer or address of the newNode */
+  *top = newNode;
 }
 
 /* passing a double pointer allows the list to be returned after the pop */
 void pop(struct stackNode **top)
 {
-    struct stackNode *ptr = *top;
-    printf("\tElement Popped Stack->[%d]\n\n", ptr->data);
-    ptr = ptr->nextPtr;
-    *top = ptr;
+  struct stackNode *ptr = *top;
+  printf("\tElement Popped Stack->[%d]\n\n", ptr->data);
+  ptr = ptr->nextPtr;
+  *top = ptr;
 }
 
 int isEmpty(struct stackNode *top)
 {
-    return top == NULL;
+  return top == NULL;
 }
 
 #endif
