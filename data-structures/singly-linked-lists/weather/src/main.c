db81558f778e278c38f9fcce2483e48ba32d157d
Patrick Mims <patrickkevinmims@gmail.com>
2022-04-23 18:37:02 -0700

    init commit

---
+++ data-structures/singly-linked-lists/weather/src/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+  printf("weather data linked list");
+  printf("1. get today's data from HERE and store it");
+  printf("use a linked list to store the date of the data ");
+  printf("search");
+
+  return 0;
+}
